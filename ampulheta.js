4eb8cc9b405d496ea3b67d228be72edade60ad39
MiDrAjX <jeisonmarques78@gmail.com>
2022-09-29 00:47:32 -0300

    Finalizando logica para desenhar ampulheta cheia no terminal

---
+++ ampulheta.js
@@ -0,0 +1,34 @@
+
+function drawHourglass(size){
+  let distAst = 0;
+  for (var linha=1; linha<=size; linha++){
+    for (var coluna=1; coluna<=size; coluna++){
+      if(coluna ===1 || coluna ===size){
+        process.stdout.write("#")
+        continue
+      }
+      if(linha === 1 || linha === size)  {
+        process.stdout.write("#")
+        continue
+      }
+    
+      if ((coluna == (1+distAst) )|| coluna == (size-distAst) || (1+distAst)<=coluna && coluna<=(size-distAst))
+      process.stdout.write("#")
+      else
+      process.stdout.write(" ");
+    }
+    process.stdout.write('\n');
+
+    distAst++;
+  }
+}
+
+process.stdin.on('readable', ()=>{ 
+   
+  let variable = process.stdin.read(); 
+  const variableParse= Number(variable)
+  if(variableParse !== NaN){
+    return drawHourglass(Number(variable))
+  }
+  
+})
